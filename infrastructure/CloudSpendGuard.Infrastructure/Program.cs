bf763a78e98b3da0d5ce9139b25a40353bdd250b
Csaba Boros <boros.csaba94@gmail.com>
2026-09-01 13:11:13 +0200

    Default tagging

--- infrastructure/CloudSpendGuard.Infrastructure/Program.cs
+++ infrastructure/CloudSpendGuard.Infrastructure/Program.cs
@@ -2,5 +2,10 @@ using Amazon.CDK;
 using CloudSpendGuard.Infrastructure;
 
 var app = new App();
+
+Tags.Of(app).Add("Project", "CloudSpendGuard");
+Tags.Of(app).Add("Environment", "Production");
+Tags.Of(app).Add("ManagedBy", "CDK");
+
 new CloudSpendGuardStack(app, "CloudSpendGuardStack");
 app.Synth();
